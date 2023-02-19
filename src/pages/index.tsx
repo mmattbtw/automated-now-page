fe4a10f41352f3125a7c3dfaec8f6396cd424b99
mmattbtw <matt@mmatt.net>
2023-02-18 19:52:14 -0600

    asdf'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import styles from "@/styles/Home.module.css";
-import { Inter } from "@next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
@@ -13,8 +8,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           <a href="https://mm.omg.lol/now">mm.omg.lol/now</a>
         </h1>
       </main>
